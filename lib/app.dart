38e1e9e5cc622bd7f3a6c8bc890c67893c7abf1a
Victor <vctudo.27@gmail.com>
2023-05-10 17:00:03 -0300

    🎨

--- lib/app.dart
+++ lib/app.dart
@@ -1,62 +1,17 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:geniopay_test/view/page.dart';
+import 'package:geniopay_test/view/style/color.dart';
+import 'package:geniopay_test/view/style/font.dart';
 
 class App extends MaterialApp {
   const App({super.key});
 
   @override
-  Widget get home => Container();
+  Widget get home => MainPage();
 
   @override
   ThemeData get theme => ThemeData(
-        textTheme: TextTheme(
-          bodyLarge: newFont(fontSize: 20),
-          bodyMedium: newFont(fontSize: 14),
-          bodySmall: newFont(fontSize: 13),
-        ),
-      );
-
-  TextStyle newFont({
-    TextStyle? textStyle,
-    Color? color,
-    Color? backgroundColor,
-    double? fontSize,
-    FontWeight? fontWeight,
-    FontStyle? fontStyle,
-    double? letterSpacing,
-    double? wordSpacing,
-    TextBaseline? textBaseline,
-    double? height,
-    Locale? locale,
-    Paint? foreground,
-    Paint? background,
-    List<Shadow>? shadows,
-    List<FontFeature>? fontFeatures,
-    TextDecoration? decoration,
-    Color? decorationColor,
-    TextDecorationStyle? decorationStyle,
-    double? decorationThickness,
-  }) =>
-      GoogleFonts.ibmPlexSans(
-        textStyle: textStyle,
-        color: color,
-        backgroundColor: backgroundColor,
-        fontSize: fontSize,
-        fontWeight: fontWeight,
-        fontStyle: fontStyle,
-        letterSpacing: letterSpacing,
-        wordSpacing: wordSpacing,
-        textBaseline: textBaseline,
-        height: height,
-        locale: locale,
-        foreground: foreground,
-        background: background,
-        shadows: shadows,
-        fontFeatures: fontFeatures,
-        decoration: decoration,
-        decorationColor: decorationColor,
-        decorationStyle: decorationStyle,
-        decorationThickness: decorationThickness,
+        colorScheme: AppColors.appColors,
+        textTheme: AppFont.appFonts,
       );
 }
