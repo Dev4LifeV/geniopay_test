8423514e00195e56e363ae2cc2d5e8e6531dddf0
Victor <vctudo.27@gmail.com>
2023-05-09 21:26:33 -0300

    💄 created step_indicator

---
+++ lib/view/widget/indicator/step_indicator.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StepIndicator extends StatelessWidget {
+  const StepIndicator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
