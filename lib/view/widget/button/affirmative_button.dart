27a87782a468e0b9c02a7ec34aa3766d145bba94
Victor <vctudo.27@gmail.com>
2023-05-10 19:17:21 -0300

    💄 created affirmative_button

---
+++ lib/view/widget/button/affirmative_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:geniopay_test/core/config/extension/ext_style.dart';
+
+class AffirmativeButton extends StatelessWidget {
+  const AffirmativeButton({
+    required this.onPressed,
+    required this.content,
+    Key? key,
+  }) : super(key: key);
+
+  final Function() onPressed;
+  final String content;
+
+  @override
+  Widget build(BuildContext context) => ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: context.colors.secondary,
+          elevation: 0,
+          splashFactory: NoSplash.splashFactory,
+        ),
+        child: Text(
+          content.toUpperCase(),
+          style: context.fonts.bodyMedium?.copyWith(fontWeight: FontWeight.w700),
+        ),
+      );
+}
