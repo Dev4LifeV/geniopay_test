605c4b54ae8d3d1d13b97dd4481c1555510acdd8
Victor <vctudo.27@gmail.com>
2023-05-10 22:06:59 -0300

    🚚 page -> view_transfer_created & 💄 NoSplash

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geniopay_test/view/page.dart';
+import 'package:geniopay_test/view/view_transfer_created.dart';
 import 'package:geniopay_test/view/style/color.dart';
 import 'package:geniopay_test/view/style/font.dart';
 
@@ -7,11 +7,12 @@ class App extends MaterialApp {
   const App({super.key});
 
   @override
-  Widget get home => MainPage();
+  Widget get home => ViewTransferCreated();
 
   @override
   ThemeData get theme => ThemeData(
         colorScheme: AppColors.appColors,
         textTheme: AppFont.appFonts,
+        splashFactory: NoSplash.splashFactory,
       );
 }
