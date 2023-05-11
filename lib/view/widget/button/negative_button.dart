2bd8ecf8bf3c6e8c54b8463a5d30d17487704117
Victor <vctudo.27@gmail.com>
2023-05-10 22:06:10 -0300

    💄 created negative_button

---
+++ lib/view/widget/button/negative_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:geniopay_test/core/config/extension/ext_style.dart';
+
+class NegativeButton extends StatelessWidget {
+  const NegativeButton({
+    required this.onPressed,
+    required this.content,
+    Key? key,
+  }) : super(key: key);
+
+  final String content;
+  final Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) => TextButton(
+        onPressed: onPressed,
+        child: Text(
+          content.toUpperCase(),
+        ),
+    style: TextButton.styleFrom(
+      splashFactory: NoSplash.splashFactory,
+          foregroundColor: context.colors.onSecondary
+    ),
+      );
+}
