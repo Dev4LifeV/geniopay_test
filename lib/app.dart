fdc4e6e88b9f0deee2fe86ccd3cae25cd5e3a81b
Victor <vctudo.27@gmail.com>
2023-05-11 00:15:44 -0300

    💄 removed card elevation

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geniopay_test/view/view_referral.dart';
 import 'package:geniopay_test/view/view_transfer_created.dart';
 import 'package:geniopay_test/view/style/color.dart';
 import 'package:geniopay_test/view/style/font.dart';
@@ -7,10 +8,11 @@ class App extends MaterialApp {
   const App({super.key});
 
   @override
-  Widget get home => ViewTransferCreated();
+  Widget get home => const ViewReferral();
 
   @override
   ThemeData get theme => ThemeData(
+        cardTheme: const CardTheme(elevation: 0),
         colorScheme: AppColors.appColors,
         textTheme: AppFont.appFonts,
         splashFactory: NoSplash.splashFactory,
