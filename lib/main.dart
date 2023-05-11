1b99b38103ae4210d57e4eeefc1a9d47709775c0
Victor <vctudo.27@gmail.com>
2023-05-11 00:37:54 -0300

    ✨MaterialApp separated due to Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:geniopay_test/app.dart';
+import 'package:geniopay_test/app_selector.dart';
 
-void main() => runApp(const App());
+void main() => runApp(const AppSelector());
